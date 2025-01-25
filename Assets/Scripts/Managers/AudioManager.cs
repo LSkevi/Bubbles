3d09f948786a31fde7d12827be1c56d259f61288
Impietuoso <palidoman@gmail.com>
2025-01-25 03:38:34 -0300

    Cena de Menu

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,41 +1,82 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class AudioManager : MonoBehaviour
-{
+public class AudioManager : MonoBehaviour {
     public static AudioManager Instance;
 
     [Header("Components")]
-    public AudioSource audioSource;
+    public AudioMixer mixer;
+    public AudioSource bgmAudioSource;
+    public AudioSource sfxAudioSource;
+    public Slider masterSlider;
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+    public TextMeshProUGUI masterText;
+    public TextMeshProUGUI bgmText;
+    public TextMeshProUGUI sfxText;
 
-    private void Awake()
-    {
+    private void Awake() {
         // Garante que apenas um AudioManager exista no jogo
-        if (Instance == null)
-        {
+        if (Instance == null) {
             Instance = this;
             DontDestroyOnLoad(gameObject); // Mantém o AudioManager entre cenas
-        }
-        else
-        {
+        } else {
             Destroy(gameObject);
         }
+
+        LoadVolumes();
     }
 
-    public void PlayAudio(AudioClip clip, bool loop = false)
-    {
-        if (audioSource == null || clip == null) return;
-        if (audioSource.isPlaying && audioSource.clip == clip) return;
+    public void PlayAudio(AudioClip clip, bool loop = false) {
+        if (bgmAudioSource == null || clip == null) return;
+        if (bgmAudioSource.isPlaying && bgmAudioSource.clip == clip) return;
 
-        audioSource.clip = clip;
-        audioSource.loop = loop;
-        audioSource.Play();
+        bgmAudioSource.clip = clip;
+        bgmAudioSource.loop = loop;
+        bgmAudioSource.Play();
     }
 
-    public void StopAudio()
-    {
-        if (audioSource.isPlaying)
-        {
-            audioSource.Stop();
+    public void StopAudio() {
+        if (bgmAudioSource.isPlaying) {
+            bgmAudioSource.Stop();
         }
+    }
+
+    public void PlaySFX(AudioClip clip) {
+        if (sfxAudioSource == null || clip == null) return;
+
+        sfxAudioSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float volume) {
+        masterText.text = "Master - " + (volume*100).ToString("F0");
+
+        mixer.SetFloat("Master Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Master Volume", volume);
+    }
+
+    public void ChangeBGMVolume(float volume) {
+        bgmText.text = "BGM - " + (volume * 100).ToString("F0");
+
+        mixer.SetFloat("BGM Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("BGM Volume", volume);
+    }
+
+    public void ChangeSFXVolume(float volume) {
+        sfxText.text = "SFX - " + (volume * 100).ToString("F0");
+
+        mixer.SetFloat("SFX Volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFX Volume", volume);
+    }
+
+    public void LoadVolumes() {
+        masterSlider.SetValueWithoutNotify(PlayerPrefs.GetFloat("Master Volume"));
+        ChangeMasterVolume(masterSlider.value);
+        bgmSlider.SetValueWithoutNotify(PlayerPrefs.GetFloat("BGM Volume"));
+        ChangeBGMVolume(bgmSlider.value);
+        sfxSlider.SetValueWithoutNotify(PlayerPrefs.GetFloat("SFX Volume"));
+        ChangeSFXVolume(sfxSlider.value);
     }
 }
