eb190106cb122a1a88ec9642c67ea815945851a9
Matheus Mendes <104966152+MendesMat@users.noreply.github.com>
2025-01-26 21:28:56 -0300

    jump function

--- Assets/Scripts/Camera/CameraSwitch.cs
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -37,25 +37,3 @@ public class CameraSwitch : MonoBehaviour
         cameraNoise.AmplitudeGain = 0f;
     }
 }
-
-//void OnTriggerEnter2D(Collider2D collision)
-//{
-//    if (collision.CompareTag("CameraBounds"))
-//        SwitchCameraFocus(collision);
-//}
-
-//void SwitchCameraFocus(Collider2D collider)
-//{
-//    if (myCamera != null)
-//    {
-//        Debug.Log($"Mudei para área: {collider.gameObject.name}");
-
-//        myCamera.transform.position = 
-//            new Vector3(collider.transform.position.x, 
-//            collider.transform.position.y,
-//            myCamera.transform.position.z);
-
-//        var orthoSize = Mathf.Max(collider.bounds.size.x, collider.bounds.size.y) / 2f;
-//        myCamera.Lens.OrthographicSize = orthoSize;
-//    }
-//}
